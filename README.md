4e65ba33e0dc61a120e42858c5759ee309436f4c
unknown <ygorpontelo@gmail.com>
2023-05-02 23:25:44 -0300

    initial version

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
-# discord_stream_bot
-Discord bot that streams audio from available input devices
+# Discord Stream Bot
+
+Once you configure your own personal bot, [see tutorial here](https://discordpy.readthedocs.io/en/stable/discord.html), place the token in a txt file in the same folder called token.txt.
+
+This bot uses Pyaudio to read any available input and stream the audio to discord. Only mono and stereo inputs are supported (1 and 2 channels). The script will present a nice interface to select those.
+
+This bot does not create a device input, only reads available ones. If you want to redirect audio using virtual inputs, you need an interface to do that, like [Jack audio](https://jackaudio.org/) on Linux or [VB-Cable](https://vb-audio.com/Cable/) on Windows.
+
+Poetry is the recommended way to manage dependencies, but you can install them via pip normally with requirements.txt (only includes main dependencies).
---
+++ main.py
@@ -0,0 +1,128 @@
+import discord
+import pyaudio
+import inquirer
+import samplerate
+import numpy as np
+
+from discord.ext import commands
+
+
+class PyAudioPCM(discord.AudioSource):
+
+    def __init__(self, device, audio) -> None:
+        super().__init__()
+        self.d = device
+        self.ratio = 48000/self.d["defaultSampleRate"]
+        self.channels = self.d["maxInputChannels"]
+        self.chunk = int(self.d["defaultSampleRate"] * 0.02)
+        self.stream = audio.open(
+            format=pyaudio.paInt16,
+            channels=self.channels,
+            rate=int(self.d["defaultSampleRate"]),
+            input=True,
+            input_device_index=self.d["index"],
+            frames_per_buffer=self.chunk,
+        )
+        self.resampler = None
+        if self.ratio != 1:
+            self.resampler = samplerate.Resampler("sinc_best", channels=2)
+
+    def read(self) -> bytes:
+        frame = self.stream.read(self.chunk, exception_on_overflow=False)
+        frame = np.frombuffer(frame, dtype=np.int16)
+        if self.channels == 1:
+            frame = np.repeat(frame, 2)
+        if self.resampler:
+            frame = np.stack((frame[::2], frame[1::2]) , axis=1)
+            return self.resampler.process(frame, self.ratio).astype(np.int16).tobytes()
+        return frame.tobytes()
+
+
+def create_bot(device, audio) -> commands.Bot:
+    intents = discord.Intents.default()
+    intents.message_content = True
+    bot = commands.Bot(command_prefix="!", description="Discord Audio Stream Bot", intents=intents)
+
+    @bot.event
+    async def on_ready():
+        print(f'Logged in as {bot.user} (ID: {bot.user.id})')
+        print('------')
+
+    @bot.command(name="join", aliases=["j"], help="Join user channel")
+    async def join(ctx):
+        """Joins a voice channel"""
+
+        channel = ctx.author.voice.channel
+        if ctx.voice_client is not None:
+            return await ctx.voice_client.move_to(channel)
+
+        await channel.connect()
+    
+    @bot.command(name="play", aliases=["p"], help="Play audio")
+    async def play(ctx):
+        """Streams sound from audio device"""
+
+        async with ctx.typing():
+            ctx.voice_client.play(PyAudioPCM(device, audio), after=lambda e: print(f'Player error: {e}') if e else None)
+
+        await ctx.send('Streaming From VB Cable')
+    
+    @bot.command(name="volume", aliases=["v"], help="Change bot volume")
+    async def volume(ctx, volume: int):
+        """Changes the player's volume"""
+
+        if ctx.voice_client is None:
+            return await ctx.send("Not connected to a voice channel.")
+
+        ctx.voice_client.source.volume = volume / 100
+        await ctx.send(f"Changed volume to {volume}%")
+    
+    @bot.command(name="stop", aliases=["s"], help="Disconnect bot")
+    async def stop(ctx):
+        """Stops and disconnects the bot from voice"""
+
+        await ctx.voice_client.disconnect()
+    
+    @play.before_invoke
+    async def ensure_voice(ctx):
+        if ctx.voice_client is None:
+            if ctx.author.voice:
+                await ctx.author.voice.channel.connect()
+            else:
+                await ctx.send("You are not connected to a voice channel.")
+                raise commands.CommandError("Author not connected to a voice channel.")
+        elif ctx.voice_client.is_playing():
+            ctx.voice_client.stop()
+    
+    return bot
+
+
+if __name__ == "__main__":
+    with open("token.txt", 'r') as f:
+        token = f.readline()
+
+    p = pyaudio.PyAudio()
+
+    # get all available inputs, mono or stereo, with all possible drivers
+    # some of those do not work, test it to check
+    device_inputs, index = {}, 1
+    for i in range(p.get_device_count()):
+        d = p.get_device_info_by_index(i)
+        if 0 < d["maxInputChannels"] < 3:
+            api = p.get_host_api_info_by_index(d["hostApi"])["name"]
+            name = f'{index} - Device: {d["name"]}, Channels: {d["maxInputChannels"]}, API: {api}'
+            device_inputs[name] = d
+            index += 1
+
+    # choice of input by user
+    questions = [
+        inquirer.List(
+            'device',
+            message="Select Device you want to stream",
+            choices=list(device_inputs),
+        ),
+    ]
+    answer = inquirer.prompt(questions)
+
+    # start bot
+    create_bot(device_inputs[answer["device"]], p).run(token)
